9c73de1a079ee3bae359f11c9f0fc1187104fa4f
Mohamed Anfossi <maroc-hamza2016@hotmail.com>
2022-10-10 14:16:40 +0100

    ADD Breeze to project

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,5 @@
 <?php
 
-use App\Http\Livewire\CartComponent;
-use App\Http\Livewire\CheckoutComponent;
-use App\Http\Livewire\HomeComponent;
-use App\Http\Livewire\ShopComponent;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,11 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/', HomeComponent::class)->name('home.index');
-Route::get('/cart', CartComponent::class)->name('cart');
-Route::get('/shop', ShopComponent::class)->name('shop');
-Route::get('/checkout', CheckoutComponent::class)->name('checkout');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
